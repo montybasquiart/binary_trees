bc45873b32e0399722ea636460a7bfe0875d3b2b
Mathias Owa Martins <owamathias@gmail.com>
2024-06-07 01:11:54 +0100

    Fixed code output error

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,10 +12,10 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-
+    
     if (parent == NULL)
         return (NULL);
+binary_tree_t *new_node = malloc(sizeof(binary_tree_t));      
     if (new_node == NULL)
         return (NULL);
     new_node->parent = parent;
